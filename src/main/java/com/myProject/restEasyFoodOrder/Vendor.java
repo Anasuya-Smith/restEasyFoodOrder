42138ca855a6c4aae72a5e853d4976ba7334ef10
Lenovo <Lenovo@DESKTOP-H3H3NAR>
2021-08-18 17:37:23 +0530

    Underlying database structure

---
+++ src/main/java/com/myProject/restEasyFoodOrder/Vendor.java
@@ -0,0 +1,63 @@
+package com.myProject.restEasyFoodOrder;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Vendor {
+	
+	private Integer vendorID;
+	private String vendorName;
+	private String dishName;
+	private float vendorDishPrice;
+	
+	// Super constructor
+	public Vendor() {
+	}
+	
+	// Constructor
+	public Vendor(Integer vendorID, String vendorName, String dishName, float vendorDishPrice) {
+		super();
+		this.vendorID = vendorID;
+		this.vendorName = vendorName;
+		this.dishName = dishName;
+		this.vendorDishPrice = vendorDishPrice;
+	}
+
+	// Getter and Setter methods
+	
+	@Id
+	public Integer getVendorID() {
+		return vendorID;
+	}
+
+	public void setVendorID(Integer vendorID) {
+		this.vendorID = vendorID;
+	}
+
+	public String getVendorName() {
+		return vendorName;
+	}
+
+	public void setVendorName(String vendorName) {
+		this.vendorName = vendorName;
+	}
+
+	public String getDishName() {
+		return dishName;
+	}
+
+	public void setDishName(String dishName) {
+		this.dishName = dishName;
+	}
+
+	public float getVendorDishPrice() {
+		return vendorDishPrice;
+	}
+
+	public void setVendorDishPrice(float vendorDishPrice) {
+		this.vendorDishPrice = vendorDishPrice;
+	}
+	
+	
+}
