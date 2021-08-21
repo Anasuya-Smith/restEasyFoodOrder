592a979a7e4e0ded65e9f0b160db9e7bcc5dc437
A Anasuya Threse Innocent <55600315+Anasuya-Smith@users.noreply.github.com>
2021-08-22 01:51:04 +0530

    Delete duplicate project directory

--- restEasyFoodOrder/src/main/java/com/myProject/restEasyFoodOrder/Repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myProject.restEasyFoodOrder.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.myProject.restEasyFoodOrder.Model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer>{
-
-}
--- restEasyFoodOrder/src/main/java/com/myProject/restEasyFoodOrder/Repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myProject.restEasyFoodOrder.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.myProject.restEasyFoodOrder.Model.Orders;
-
-public interface CustomerRepository extends JpaRepository<Orders, Integer> {
-
-}
